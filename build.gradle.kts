182b761b1ce6d529e2e870298f82c486e20d0761
orange-buffalo <orange-buffalo@users.noreply.github.com>
2024-01-27 10:32:34 +1100

    build: enabling build scan

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,3 +35,10 @@ subprojects {
         }
     }
 }
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,7 @@
+plugins {
+    id("com.gradle.enterprise") version ("3.13.2")
+}
+
 rootProject.name = "testcontainers-playwright-assembly-tests"
 include("java-basic")
 include("java-custom-playwright")
