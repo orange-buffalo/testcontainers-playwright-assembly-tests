32422bd745340868218fdeb57d9f3a72967fe08f
orange-buffalo <orange-buffalo@users.noreply.github.com>
2024-01-27 10:32:08 +1100

    tests: Changing title assertion to be resilient against description changes

--- java-custom-playwright/src/test/java/io/orangebuffalo/testcontainersplaywrightsamples/BasicTest.java
+++ java-custom-playwright/src/test/java/io/orangebuffalo/testcontainersplaywrightsamples/BasicTest.java
@@ -3,6 +3,7 @@ package io.orangebuffalo.testcontainersplaywrightsamples;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.assertions.PlaywrightAssertions;
 import io.orangebuffalo.testcontainers.playwright.junit.PlaywrightExtension;
+import java.util.regex.Pattern;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -12,6 +13,7 @@ class BasicTest {
     @Test
     void testBasic(Page page) {
         page.navigate("https://github.com/orange-buffalo/testcontainers-playwright");
-        PlaywrightAssertions.assertThat(page).hasTitle("GitHub - orange-buffalo/testcontainers-playwright");
+        PlaywrightAssertions.assertThat(page)
+                .hasTitle(Pattern.compile(".*orange-buffalo/testcontainers-playwright.*"));
     }
 }
--- kotlin-basic/src/test/kotlin/io/orangebuffalo/testcontainersplaywrightsamples/BasicTest.kt
+++ kotlin-basic/src/test/kotlin/io/orangebuffalo/testcontainersplaywrightsamples/BasicTest.kt
@@ -5,6 +5,7 @@ import com.microsoft.playwright.assertions.PlaywrightAssertions
 import io.orangebuffalo.testcontainers.playwright.junit.PlaywrightExtension
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import java.util.regex.Pattern
 
 @ExtendWith(PlaywrightExtension::class)
 class BasicTest {
@@ -12,6 +13,7 @@ class BasicTest {
     @Test
     fun testBasic(page: Page) {
         page.navigate("https://github.com/orange-buffalo/testcontainers-playwright")
-        PlaywrightAssertions.assertThat(page).hasTitle("GitHub - orange-buffalo/testcontainers-playwright")
+        PlaywrightAssertions.assertThat(page)
+                .hasTitle(Pattern.compile(".*orange-buffalo/testcontainers-playwright.*"));
     }
 }
